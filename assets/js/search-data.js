b7a2d280a79512d7fdfbc755b8f05cc8886ce60f
Helga1970 <olga.kostrikina@gmail.com>
2026-01-30 18:39:00 +0100

    Update search-data.js

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -2,18 +2,13 @@
 layout: none
 ---
 window.store = {
-  {% assign all_items = site.recipes | concat: site.books %}
-  {% for item in all_items %}
-    "item-{{ forloop.index }}": {
-      "type": "{% if item.layout == 'recipe' or item.recipe %}recipe{% else %}book{% endif %}",
-      "title": {{ item.title | jsonify }},
-      "category": {{ item.categories | join: ', ' | jsonify }},
-      "url": "{{ item.url | relative_url }}",
-      "featured_image": "{{ item.featured_image | relative_url }}",
-      "cover": "{{ item.cover | relative_url }}",
-      "book_title": {{ item.book.title | jsonify }},
-      "content": {{ item.content | strip_html | strip_newlines | truncatewords: 50 | jsonify }},
-      "content_preview": {{ item.content | strip_html | truncatewords: 15 | jsonify }}
+  {% for post in site.recipes %}
+    "recipe-{{ forloop.index }}": {
+      "type": "recipe",
+      "title": {{ post.title | jsonify }},
+      "url": "{{ post.url | relative_url }}",
+      "featured_image": "{{ post.featured_image | relative_url }}",
+      "content": {{ post.content | strip_html | strip_newlines | truncatewords: 20 | jsonify }}
     }{% unless forloop.last %},{% endunless %}
   {% endfor %}
 };
