14d8b57907ab5d12989a4304797ab421476ffcf6
Helga1970 <olga.kostrikina@gmail.com>
2026-01-31 15:16:15 +0100

    Update search-data.js

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -8,7 +8,8 @@ window.store = {
       "title": {{ post.title | jsonify }},
       "url": "{{ post.url | relative_url }}",
       "featured_image": "{{ post.featured_image | relative_url }}",
-      "category": {{ post.categories | join: ', ' | jsonify }},
+      "categories": {{ post.categories | jsonify }},
+      "tags": {{ post.tags | jsonify }},
       "content": {{ post.content | strip_html | strip_newlines | truncatewords: 100 | jsonify }}
     }{% unless forloop.last %},{% endunless %}
   {% endfor %}
