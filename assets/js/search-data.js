46056f6a8d07911d9fb061e828dcd74078c543b4
Helga1970 <olga.kostrikina@gmail.com>
2026-02-01 19:56:33 +0100

    Update search-data.js

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,19 +4,15 @@ layout: none
 window.store = {
   {% for post in site.recipes %}
     "recipe-{{ forloop.index }}": {
-      "type": "recipe",
       "title": {{ post.title | jsonify }},
       "url": "{{ post.url | relative_url }}",
       "featured_image": "{{ post.featured_image | relative_url }}",
       "categories": {{ post.categories | jsonify }},
       "tags": {{ post.tags | jsonify }},
-      "book": {
-        "title": {{ post.book.title | jsonify }},
-        "slug": {{ post.book.title_slug | jsonify }},
-        "cover": "{{ post.book.cover | relative_url }}",
-        "link": "{{ post.book.link | jsonify }}"
-      },
-      "content": {{ post.content | strip_html | strip_newlines | truncatewords: 100 | jsonify }}
+      "book_title": {{ post.book.title | jsonify }},
+      "book_slug": {{ post.book.title_slug | jsonify }},
+      "book_cover": "{{ post.book.cover | relative_url }}",
+      "book_link": "{{ post.book.link }}"
     }{% unless forloop.last %},{% endunless %}
   {% endfor %}
 };
